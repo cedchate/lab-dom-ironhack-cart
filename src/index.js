6c8319fa868209120042a11c721ee079c104987d
Cedric Chatellier <cedricchatellier95@gmail.com>
2023-01-27 16:47:34 +0100

    bonus

--- src/index.js
+++ src/index.js
@@ -35,7 +35,9 @@ function calculateAll() {
 // ITERATION 4
 
 	function removeProduct(event) {
-	  const target = event.currentTarget;
+	  const target = event.currentTarget.parentNode.parentNode;
+    const lineTab= target.parentNode;
+    const removedLine= lineTab.removeChild(target);
 	  console.log('The target in remove is:', target);
   //... your code goes here
 }
@@ -44,11 +46,70 @@ function calculateAll() {
 
 function createProduct() {
   //... your code goes here
+  newLineElement= document.createElement('tr');
+  newLineElement.classList.add('product');
+
+  prodNameElement= document.createElement('td');
+  prodPriceElement= document.createElement('td');
+  prodQuantityElement= document.createElement('td');
+  prodTotalElement= document.createElement('td');
+  prodBtnElement= document.createElement('td');
+
+  newLineElement.append(prodNameElement);
+  nameSpan= document.createElement('span');
+  nameSpan.innerHTML= document.querySelector('.create-product>td:first-child>input').value;
+  prodNameElement.classList.add('name');
+  prodNameElement.append(nameSpan);
+  
+  newLineElement.append(prodPriceElement);
+  prodPriceElement.classList.add('price');
+  prodPriceElement.innerHTML= "$";
+  priceElement= document.createElement("span");
+  priceElement.innerHTML= document.querySelector('.create-product>td:nth-child(2)>input').value;
+  prodPriceElement.append(priceElement);
+  
+  newLineElement.append(prodQuantityElement);
+  prodQuantityElement.classList.add('quantity');
+  chooseQuantityElement= document.createElement('input');
+  chooseQuantityElement.setAttribute('type', 'number');
+  chooseQuantityElement.setAttribute('value', '0');
+  chooseQuantityElement.setAttribute('min', '0');
+  chooseQuantityElement.setAttribute('placeholder', 'Quantity');
+  prodQuantityElement.append(chooseQuantityElement);
+
+  newLineElement.append(prodTotalElement);
+  prodTotalElement.classList.add("subtotal");
+  totalSpan= document.createElement('span');
+  totalSpan.innerHTML= 0;
+  prodTotalElement.append(totalSpan);
+
+  newLineElement.append(prodBtnElement);
+  prodBtnElement.classList.add('action');
+  BtnElement= document.createElement('button');
+  BtnElement.classList.add('btn');
+  BtnElement.classList.add('btn-remove');
+  BtnElement.innerHTML= 'Remove';
+  prodBtnElement.append(BtnElement);
+
+  tbodyElement= document.querySelector('tbody');
+  tbodyElement.append(newLineElement);
+ 
+  let removeBtn= document.querySelectorAll('.btn-remove');
+  removeBtn.forEach((btn)=> {
+    btn.addEventListener('click', removeProduct);
+  });
 }
 
 window.addEventListener('load', () => {
+  const createBtn= document.querySelector('#create');
+  createBtn.addEventListener('click', createProduct);
+
+  let removeBtn= document.querySelectorAll('.btn-remove');
+  removeBtn.forEach((btn)=> {
+    btn.addEventListener('click', removeProduct);
+  });
+  
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
-
   //... your code goes here
 });
